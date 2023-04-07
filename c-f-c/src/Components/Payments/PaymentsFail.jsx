74efe7be38c820d9273a09f8c8798ccf3125d5fa
SARB <mrsinghbusiness05@gmail.com>
2023-04-07 18:17:00 +0530

    feature - Payment Fail UI Done

--- c-f-c/src/Components/Payments/PaymentsFail.jsx
+++ c-f-c/src/Components/Payments/PaymentsFail.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+
+import { Button, Container, Heading, VStack } from '@chakra-ui/react';
+
+import { RiErrorWarningFill } from 'react-icons/ri';
+import { Link } from 'react-router-dom';
 
 const PaymentsFail = () => {
   return (
-    <div>PaymentsFail</div>
+    <div>
+       <Container h = "90vh">
+            <VStack justifyContent = {'center'} h="full" spacing={'4'}>
+              <RiErrorWarningFill size={'5rem'} />
+              <Heading> Payment Failed </Heading>
+              <Link to="/">
+                <Button variant={'ghost'}>Go to home</Button>
+              </Link>
+            </VStack>
+       </Container>
+    </div>
   )
 }
 
