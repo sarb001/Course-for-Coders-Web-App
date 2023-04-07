aea6225db9d174b2dd962935f463a88ee0d755f0
SARB <mrsinghbusiness05@gmail.com>
2023-04-07 17:19:05 +0530

    feature - About + Course Request Component Created

--- c-f-c/src/App.js
+++ c-f-c/src/App.js
@@ -8,6 +8,9 @@ import Login from './Components/Auth/Login';
 import Register from './Components/Auth/Register';
 import ForgetPassword from './Components/Auth/ForgetPassword';
 import ResetPassword from './Components/Auth/ResetPassword';
+import Contact from './Components/Contact/Contact';
+import Request from './Components/Request/Request';
+import About from './Components/About/About';
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
             <Route exact path = "/"  element= {<Home />}> </Route>
             <Route exact path = "/courses"  element= {<Courses />}> </Route>
             <Route exact path = "/login"  element= {<Login />}> </Route>
+            <Route exact path = "/contact"  element= {<Contact  />}> </Route>
+            <Route exact path = "/request"  element= {<Request  />}> </Route>
+            <Route exact path = "/about"  element= {<About  />}> </Route>
             <Route exact path = "/register"  element= {<Register />}> </Route>
             <Route exact path = "/forgetpassword"  element= {<ForgetPassword />}> </Route>
             <Route exact path = "/resetpassword/:token"  element= {<ResetPassword />}> </Route>
