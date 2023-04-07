4c5f225101f9348b12732003c342aea67807249b
SARB <mrsinghbusiness05@gmail.com>
2023-04-07 11:14:05 +0530

    feature - Signup/Login UI Created

--- c-f-c/src/App.js
+++ c-f-c/src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router , Route ,Routes } from 'react-router-dom'
 import Home from './Components/Home/Home';
 import Header from './Components/Layout/Header/Header';
 import Courses from './Components/Courses/Courses';
+import Footer from './Components/Layout/Footer/Footer';
+import Login from './Components/Auth/Login';
+import Register from './Components/Auth/Register';
+import ForgetPassword from './Components/Auth/ForgetPassword';
 
 function App() {
   return (
@@ -12,7 +16,11 @@ function App() {
          <Routes>
             <Route exact path = "/"  element= {<Home />}> </Route>
             <Route exact path = "/courses"  element= {<Courses />}> </Route>
+            <Route exact path = "/login"  element= {<Login />}> </Route>
+            <Route exact path = "/register"  element= {<Register />}> </Route>
+            <Route exact path = "/forgetpassword"  element= {<ForgetPassword />}> </Route>
          </Routes>
+         <Footer />
      </Router>
      </>
   );
