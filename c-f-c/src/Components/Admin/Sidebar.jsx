404f1d0e4f1b4fdcf23820b0d68492b5b14f84ae
SARB <mrsinghbusiness05@gmail.com>
2023-04-09 05:03:31 +0530

    refactor -Dashboard

--- c-f-c/src/Components/Admin/Sidebar.jsx
+++ c-f-c/src/Components/Admin/Sidebar.jsx
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom'
 const Sidebar = () => {
   return (
     <div>
-      <VStack spacing = {'8'} P = '16' boxShadow = {'-2px 0 10px rgba(107,70,193.0.5)'}>
+      <VStack spacing = {'8'} P = '16' boxShadow = {'-2px 0 10px rgba(107,70,193,0.5)'}>
       <LinkButton  Icon = {RiDashboardFill}  text = "Dashboard" url = {`dashboard`} />
       <LinkButton  Icon = {RiAddCircleFill}  text = "Create Course" url = {`createcourse`} />
       <LinkButton  Icon = {RiEyeFill}        text = "Courses" url = {`courses`} />
