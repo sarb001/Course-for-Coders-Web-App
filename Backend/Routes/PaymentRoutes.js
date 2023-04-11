4439cc32699010a783b9d1bac92d8dcf070e64ea
SARB <mrsinghbusiness05@gmail.com>
2023-04-12 04:21:53 +0530

    Subscription -- Cancel/Verify

--- Backend/Routes/PaymentRoutes.js
+++ Backend/Routes/PaymentRoutes.js
@@ -1,9 +1,24 @@
 import express from 'express';
 import { isAuthenticated } from '../Middlewares/auth.js';
-import { buySubscription } from '../Controllers/PaymentController.js';
+import { buySubscription, paymentverification , razorpaykey } 
+from '../Controllers/PaymentController.js';
 
 const router = express.Router();
 
+// Buy Subscription or Subscribe 
+
 router.route('/subscribe').get(isAuthenticated,buySubscription)
+
+// Verify Payment 
+
+router.route('/paymentverification').post(isAuthenticated,paymentverification)
+
+// Cancel Subscription 
+
+router.route('/subscribe/cancel').delete(isAuthenticated,cancelSubscription)
+
+// Get the RazorPay Key
+
+router.route('/razorpaykey').get(razorpaykey)
 
 export default router;
